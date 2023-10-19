7fe153e765efa3a5684cdabfcce871b54d497229
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-10-19 04:36:50 -0700

    creation of the rotr_handler.c

---
+++ rotr_handler.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ *rotrHandler - function that rotates the stack to it's bottom
+ *@head: represents the stack head
+ *@lineCounter: represents line number
+ *
+ */
+
+void rotrHandler(stack_t **head,
+		__attribute_((unsued)) unsigned int lineCounter)
+{
+	stack_t *cpy;
+
+	cpy = *head;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return;
+	while (cpy->next)
+		cpy = cpy->next;
+	cpy->next = *head;
+	cpy->prev->next = NULL;
+	cpy->prev = NULL;
+	(*head)->prev = cpy;
+	(*had) = cpy
+}
