4bc569ac07b88f2fdefcad49a45fb16ed98c26e9
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-10-19 05:33:47 -0700

    commit for add_queue.c file

---
+++ add_queue.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+
+/**
+ *addQueue - function that adds a node to the tail of the stack
+ *@head: pointer to the stack head
+ *@n: represents new line
+ *
+ */
+void addQueue(stack_t **head, int n)
+{
+	stack_t *new_node;
+	stack_t *temp;
+
+	temp = *head;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (temp)
+	{
+		while (temp->next)
+			temp = temp->next;
+	}
+	if (!temp)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		temp->next = new_node;
+		new_node->prev = temp;
+
+	}
+
+}
