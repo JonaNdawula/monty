49a7ae39e1755691136d5e67b00a015c2506d0f1
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-10-19 05:17:27 -0700

    commit of stack_handler.c file

---
+++ stack_handler.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+/**
+ *stackHandler - function that prints the top of the stack
+ *@head: represents the stack head
+ *@lineCounter:represents the line number
+ */
+void stackHandler(stack_t **head, unsigned int lineCounter)
+{
+
+	(void) head;
+	(void) lineCounter;
+
+	bus.stackFlag = 0;
+
+
+}
